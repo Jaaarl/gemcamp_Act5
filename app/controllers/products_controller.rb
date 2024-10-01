dbe66cb75f07583376ecbbf64dd4b225c5b22e46
Jaaarl <jarl13.work@gmail.com>
2024-10-01 15:55:29 +0800

    add: routes for products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,39 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params[:product].permit(:name, :content, :quantity, :price, :available, :released_at, :discount, :image))
+    if @product.save
+      redirect_to products_path
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(params.require(:product).permit(:name, :content, :quantity, :price, :available, :released_at, :discount, :image))
+      redirect_to products_path
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      redirect_to products_path
+    end
+  end
+end
+
