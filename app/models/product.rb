0f05f95765d04344a2225c5599d121af58c67bcc
Jaaarl <jarl13.work@gmail.com>
2024-10-04 19:38:55 +0800

    add: validation discount & uniqueness

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-  validates :name, presence:true
+  validates :name, presence:true, uniqueness: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
