5bd9d89e6700b9bdd77413412c0807d9ace48578
Jaaarl <jarl13.work@gmail.com>
2024-10-09 16:18:22 +0800

    add: add review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,9 +5,27 @@ class ReviewsController < ApplicationController
     @reviews = @product.reviews
   end
 
+  def new
+    @review = @product.reviews.build
+  end
+
+  def create
+    @review = @product.reviews.build(comment_params)
+    if @review.save
+      flash[:notice] = 'review created successfully'
+      redirect_to product_reviews_path(@product)
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find params[:product_id]
+  end
+
+  def comment_params
+    params.require(:review).permit(:content, :rating)
   end
 end
