7cb28098df63833d6310c007091332e9e51858c6
Jaaarl <jarl13.work@gmail.com>
2024-10-10 15:10:42 +0800

    add: edit review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_product
+  before_action :set_review, only: [:edit, :update]
 
   def index
     @reviews = @product.reviews
@@ -10,12 +11,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = @product.reviews.build(comment_params)
+    @review = @product.reviews.build(review_params)
     if @review.save
       flash[:notice] = 'review created successfully'
       redirect_to product_reviews_path(@product)
     else
       render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @review.update(review_params)
+      flash[:notice] = 'review updated successfully'
+      redirect_to product_reviews_path(@product)
+    else
+      render :edit
     end
   end
 
@@ -25,7 +37,11 @@ class ReviewsController < ApplicationController
     @product = Product.find params[:product_id]
   end
 
-  def comment_params
+  def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_review
+    @review = @product.reviews.find(params[:id])
   end
 end
