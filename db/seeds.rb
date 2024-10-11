49ea1729438f2e585bceeac020c4508fc3d89b1a
Jaaarl <jarl13.work@gmail.com>
2024-10-11 10:16:46 +0800

    add: sample product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  Product.create(
+    name: Faker::Commerce.product_name,
+    content: Faker::Lorem.paragraph,
+    quantity: Faker::Number.between(from: 1, to: 100),
+    price: Faker::Commerce.price(range: 0.0..100.0),
+    available: Faker::Boolean.boolean,
+    released_at: Faker::Date.between(from: '2020-01-01', to: Date.today),
+    discount: Faker::Commerce.price(range: 0.0..20.0),
+    created_at: Time.now,
+    updated_at: Time.now,
+    image: Faker::Internet.url
+  )
+end
